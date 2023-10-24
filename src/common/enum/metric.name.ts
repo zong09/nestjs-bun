385c10c09ba16f694d28b74bfd6b913c03d62a32
Zong <zong09@hotmail.com>
2023-10-24 17:11:05 +0700

    update enum

--- src/common/enum/metric.name.ts
+++ src/common/enum/metric.name.ts
@@ -1,19 +1,8 @@
 export enum MetricName {
   Authentication = 'Authentication',
   JobSchedule = 'JobSchedule',
-  ApiOCSC = 'ApiOCSC',
-  Profile = 'Profile',
 }
 
 export enum MetricFunction {
   Token = 'Token',
-  StartSyncOSSC = 'StartSyncOSSC',
-  SyncOSSC = 'SyncOSSC',
-  GetAllSync = 'GetAllSync',
-  CleanSyncProfile = 'CleanSyncProfile',
-  CleanQueueData = 'CleanQueueData',
-  CleanProfileTransaction = 'CleanProfileTransaction',
-  CioInfo = 'CioInfo',
-  CioUpload = 'CioUpload',
-  Register = 'Register',
 }
